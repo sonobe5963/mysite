857d54a72383b5ce43e1a8a9a6c1632453edcdd2
ezobozu <sonobe1298@gmail.com>
2026-03-31 06:49:43 +0900

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// // https://astro.build/config
-// export default defineConfig({});
-
 export default defineConfig({
-  // あなたのGitHub PagesのURL（予定）
   site: 'https://sonobe5963.github.io',
-  // リポジトリ名が mysite なら、ここを '/mysite' にする
-  base: '/mysite',
+  base: '/mysite', // ← ここがリポジトリ名と一致している必要があります
 });
